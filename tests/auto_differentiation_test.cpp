f7cd82a07d76d7441bd7601176fb84164feef830
Yuta Hinokuma <yuta@abeja.asia>
2016-06-14 22:53:41 +0900

    test追加

--- tests/auto_differentiation_test.cpp
+++ tests/auto_differentiation_test.cpp
@@ -2,3 +2,13 @@
 // Created by Yuta Hinokuma on 2016/06/14.
 //
 
+#include <AutomaticDifferentiation/Variable.hpp>
+#include <gtest/gtest.h>
+
+TEST(basic_grad, Variable)
+{
+  NYAO_NN_VARIABLE(float) t(10);
+
+  ASSERT_FLOAT_EQ(t.get_value(), 10);
+  ASSERT_FLOAT_EQ(t.get_grad(t), 1);
+}
